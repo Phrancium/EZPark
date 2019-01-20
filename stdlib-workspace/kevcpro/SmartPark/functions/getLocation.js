e06860270ea303ed5ce88a32f5a48e2f18d298e1
Kevin Cho <kevc.pro@gmail.com>
2019-01-20 00:38:23 -0500

    Location Functinoality

---
+++ stdlib-workspace/kevcpro/SmartPark/functions/getLocation.js
@@ -0,0 +1,20 @@
+const lib = require('lib')({token: 'SbnWZYDiKNqpD8VXv_9uN4b897J-GQjJ5lz5Odfz7rs3PI_i5nJeCSxuUiruJDxG'});
+const storage = lib.utils.storage['@0.1.6'];
+/**
+* Retreives the Locations associated with all vehicles
+* @returns {any}
+*/
+module.exports = async (context) => {
+	let result = await storage.get({
+	key: "vehicle"
+});
+	var locations = [];
+	for(let i = 0; i < result.length; i++){
+	    let result1 = await storage.get({
+	   	key: result[i]
+		});
+		locations.push(result1);
+	}
+	return locations;
+
+};
